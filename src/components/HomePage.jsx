2f8ec988f3eff9283e0153e1db9759a111cb4257
Ajay Yadav <ajmeansajay879@gmail.com>
2025-08-15 16:30:20 +0530

    Initial commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
       <motion.header
         initial={{ opacity: 0, y: -22 }}
         whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true, amount: 0.7 }}
+        // viewport={{ once: true, amount: 0.7 }}
         transition={{ duration: 0.6 }}
 
         className="navbar">
